10d5b41403b1a8637a1335232c6248a013e2296d
Chris Powell <chris.ian.powell@gmail.com>
2019-07-25 12:24:01 -0600

    Init commit

---
+++ neo_getter.py
@@ -0,0 +1,21 @@
+import urllib.request as req
+import json
+from api_key import key
+
+url = 'https://api.nasa.gov/neo/rest/v1/feed?'
+
+start_date = 'start_date=2015-09-07'
+
+end_date = 'end_date=2015-09-08'
+
+api_key = 'api_key=' + key
+
+final_url = url + start_date + '&' + end_date + '&' + api_key
+
+neo = req.urlopen(final_url)
+
+req_data = neo.read()
+
+encoding = neo.info().get_content_charset('utf8')
+
+data = json.loads(req_data.decode(encoding))
